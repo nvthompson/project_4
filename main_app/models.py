26bbdad95e4aced7000a78f4debd84fa9c034e2d
Nicole Thompson <Nicole@MacBook-Pro-2.lan1>
2022-02-22 10:55:50 -0800

    adding login/logout auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     memory = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
