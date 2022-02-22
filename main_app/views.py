80e8cb94b7229d61301133e6c3265d21fbfa71d1
Nicole Thompson <Nicole@MacBook-Pro-2.lan1>
2022-02-22 08:02:30 -0800

    setting up song index page a and style additions using bootstrap

--- main_app/views.py
+++ main_app/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+class Song:
+  def __init__(self, title, artist, memory):
+    self.title = title
+    self.artist = artist
+    self.memory = memory
+
+songs = [
+  Song('Sweater Weather', 'The Neighbourhood', 'Listened to it the first time driving the high school')
+]
+
 def home(request):
-  return HttpResponse('<h1>lets jam</h1>')
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def songs_index(request):
+  return render(request, 'songs/index.html', {'songs': songs})
